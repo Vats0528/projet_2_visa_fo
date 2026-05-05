c7083499d14faa7387a43efbb82eb55dbd037adf
akapapillio <pixelshinobu@gmail.com>
2026-05-05 08:18:49 +0000

    Update api.js

--- app/front/src/services/api.js
+++ app/front/src/services/api.js
@@ -2,7 +2,12 @@ import axios from 'axios'
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1'
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.88.5:8080/api/v1'
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://10.142.60.162:8080/api/v1'
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://10.142.60.162:8080/api/v1'
+
+const currentIP = window.location.hostname;
+const shareUrl = `http://${currentIP}:5173/`;
+const API_BASE_URL =  `http://${currentIP}:8080/api/v1`;
+
 const client = axios.create({
   baseURL: API_BASE_URL,
   headers: {
